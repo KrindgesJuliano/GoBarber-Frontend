3a2e88eb5ca9f3cdf51d04bbc026abbb06458e5f
Juliano Krindges <juliano9krindges@hotmail.com>
2019-10-05 22:22:39 -0300

    feat: create and styling sign in and signup screens

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/gobarber_logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um email valido')
+    .required('O e-mail é obrigatorio'),
+  password: Yup.string()
+    .min(6, 'A senha precisa de ao menos 6 caracteres')
+    .required('A senha é obrigatoria'),
+});
 
 export default function Signup() {
-  return <div />;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+
+        <Input name="password" type="password" placeholder="senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
 }
