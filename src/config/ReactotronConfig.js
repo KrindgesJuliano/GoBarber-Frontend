41ed2f1229ae80f48f72c883a025f4574c68a9d8
Juliano Krindges <juliano9krindges@hotmail.com>
2019-10-05 22:28:08 -0300

    feat: intial config for redux

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,12 @@
 import Reactotron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSafa from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure().connect();
+  const tron = Reactotron.configure()
+    .use(reactotronRedux())
+    .use(reactotronSafa())
+    .connect();
 
   tron.clear();
 
