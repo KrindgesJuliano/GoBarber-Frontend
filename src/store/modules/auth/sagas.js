b3a50ea5b87fb22a91095afb471d582f54eed752
Juliano Krindges <juliano9krindges@hotmail.com>
2019-10-06 20:28:05 -0300

    feat: persist token auth for all requesitions

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -18,7 +18,10 @@ export function* signIn({ payload }) {
 
     if (!user.provider) {
       toast.error('Usuario não é prestador');
+      return;
     }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
 
     yield put(sigInSuccess(token, user));
 
@@ -29,4 +32,37 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha no cadastro, verifique seus dados!');
+
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
