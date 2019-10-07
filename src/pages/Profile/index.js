de9f89843980d1396df1a17f1013d6bcbfe79bfe
Juliano Krindges <juliano9krindges@hotmail.com>
2019-10-07 15:50:10 -0300

    feat: add screen update profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Email" />
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Senha Atual" />
+        <Input type="password" name="password" placeholder="Nova Senha" />
+
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Comfirme a senha"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
